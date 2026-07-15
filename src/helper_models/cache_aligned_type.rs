55e4284ff3972cf5c8088a7464632e2fc69f517f
itzArka1407 <arkaprovodas2004@gmail.com>
2026-07-15 18:27:02 +0530

    Removed the pointer's padding(provided to avoid false sharing). Since the model focuses on memory-usage the most, that was taking a lot of memory. NEXT: To improve performance, join the bit masks into a unified bit mask

--- src/helper_models/cache_aligned_type.rs
+++ src/helper_models/cache_aligned_type.rs
@@ -1,15 +1,15 @@
 // Make a cached type based on the system where the program is compiled
 
-#[cfg_attr(any(target_arch = "x86_64", target_arch = "x86"), repr(align(64)))]
-#[cfg_attr(any(target_arch = "aarch64", target_arch = "arm"), repr(align(128)))]
-#[cfg_attr(
-    not(any(
-        target_arch = "aarch64",
-        target_arch = "arm",
-        target_arch = "x86_64",
-        target_arch = "x86"
-    )),
-    repr(align(64)) // Safe fallback for cache
-)]
+// #[cfg_attr(any(target_arch = "x86_64", target_arch = "x86"), repr(align(64)))]
+// #[cfg_attr(any(target_arch = "aarch64", target_arch = "arm"), repr(align(128)))]
+// #[cfg_attr(
+//     not(any(
+//         target_arch = "aarch64",
+//         target_arch = "arm",
+//         target_arch = "x86_64",
+//         target_arch = "x86"
+//     )),
+//     repr(align(64)) // Safe fallback for cache
+// )]
 #[derive(Default)]
 pub struct CacheAligned<T: Default>(pub T);
